1a9b33ebe23486667e426c517591129fdaa870f5
Diego Guerra <dgoguerra.or@gmail.com>
2017-11-03 17:42:38 +0100

    improved support to more complex uris, closes #3

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ var tape = require('tape'),
     parseRepo = require('./index.js');
 
 tape('github remotes', function(t) {
+    // http protocol ending in .git
     t.deepEqual(parseRepo('https://github.com/npm/docs.git'), {
         remote: 'https://github.com/npm/docs.git',
         protocol: 'https',
@@ -11,6 +12,7 @@ tape('github remotes', function(t) {
         project: 'docs'
     });
 
+    // http protocol without .git
     t.deepEqual(parseRepo('https://github.com/npm/docs'), {
         remote: 'https://github.com/npm/docs',
         protocol: 'https',
@@ -20,8 +22,19 @@ tape('github remotes', function(t) {
         project: 'docs'
     });
 
+    // short notation of ssh protocol
     t.deepEqual(parseRepo('git@github.com:npm/docs.git'), {
         remote: 'git@github.com:npm/docs.git',
+        protocol: 'ssh',
+        host: 'github.com',
+        repository: 'npm/docs',
+        owner: 'npm',
+        project: 'docs'
+    });
+
+    // alternative short notation of ssh
+    t.deepEqual(parseRepo('git://github.com/npm/docs.git'), {
+        remote: 'git://github.com/npm/docs.git',
         protocol: 'git',
         host: 'github.com',
         repository: 'npm/docs',
@@ -29,15 +42,27 @@ tape('github remotes', function(t) {
         project: 'docs'
     });
 
+    // ssh protocol long notation
+    t.deepEqual(parseRepo('ssh://git@github.com:npm/docs.git'), {
+        remote: 'ssh://git@github.com:npm/docs.git',
+        protocol: 'ssh',
+        host: 'github.com',
+        repository: 'npm/docs',
+        owner: 'npm',
+        project: 'docs'
+    });
+
+    // repo with periods in name (tests issue #2)
     t.deepEqual(parseRepo('git@github.com:simshanith/pi.simloovoo.com.git'), {
         remote: 'git@github.com:simshanith/pi.simloovoo.com.git',
-        protocol: 'git',
+        protocol: 'ssh',
         host: 'github.com',
         repository: 'simshanith/pi.simloovoo.com',
         owner: 'simshanith',
         project: 'pi.simloovoo.com'
     });
 
+    // repo with periods in name (tests issue #2)
     t.deepEqual(parseRepo('https://github.com/simshanith/pi.simloovoo.com'), {
         remote: 'https://github.com/simshanith/pi.simloovoo.com',
         protocol: 'https',
@@ -47,10 +72,41 @@ tape('github remotes', function(t) {
         project: 'pi.simloovoo.com'
     });
 
+    // repo with complex protocol
+    t.deepEqual(parseRepo('git+ssh://git@github.com:npm/docs.git'), {
+        remote: 'git+ssh://git@github.com:npm/docs.git',
+        protocol: 'git+ssh',
+        host: 'github.com',
+        repository: 'npm/docs',
+        owner: 'npm',
+        project: 'docs'
+    });
+
+    // repo with complex protocol over https
+    t.deepEqual(parseRepo('git+https://isaacs@github.com/npm/docs.git'), {
+        remote: 'git+https://isaacs@github.com/npm/docs.git',
+        protocol: 'git+https',
+        host: 'github.com',
+        repository: 'npm/docs',
+        owner: 'npm',
+        project: 'docs'
+    });
+
+    // ignore trailing modifiers with '#', such as the supported on npm
+    t.deepEqual(parseRepo('git+ssh://git@github.com:npm/npm.git#v1.0.27'), {
+        remote: 'git+ssh://git@github.com:npm/npm.git#v1.0.27',
+        protocol: 'git+ssh',
+        host: 'github.com',
+        repository: 'npm/npm',
+        owner: 'npm',
+        project: 'npm'
+    });
+
     t.end();
 });
 
 tape('bitbucket remotes', function(t) {
+    // http protocol ending in .git
     t.deepEqual(parseRepo('https://user@bitbucket.org/owner/org.git'), {
         remote: 'https://user@bitbucket.org/owner/org.git',
         protocol: 'https',
@@ -60,6 +116,7 @@ tape('bitbucket remotes', function(t) {
         project: 'org'
     });
 
+    // http protocol without .git
     t.deepEqual(parseRepo('https://user@bitbucket.org/owner/org'), {
         remote: 'https://user@bitbucket.org/owner/org',
         protocol: 'https',
@@ -69,9 +126,10 @@ tape('bitbucket remotes', function(t) {
         project: 'org'
     });
 
+    // ssh protocol
     t.deepEqual(parseRepo('git@bitbucket.org:owner/org.git'), {
         remote: 'git@bitbucket.org:owner/org.git',
-        protocol: 'git',
+        protocol: 'ssh',
         host: 'bitbucket.org',
         repository: 'owner/org',
         owner: 'owner',
@@ -82,6 +140,7 @@ tape('bitbucket remotes', function(t) {
 });
 
 tape('gitlab remotes', function(t) {
+    // http protocol ending in .git
     t.deepEqual(parseRepo('https://gitlab.com/gitlab-org/gitlab-ce.git'), {
         remote: 'https://gitlab.com/gitlab-org/gitlab-ce.git',
         protocol: 'https',
@@ -91,9 +150,10 @@ tape('gitlab remotes', function(t) {
         project: 'gitlab-ce'
     });
 
+    // ssh protocol
     t.deepEqual(parseRepo('git@gitlab.com:gitlab-org/gitlab-ce.git'), {
         remote: 'git@gitlab.com:gitlab-org/gitlab-ce.git',
-        protocol: 'git',
+        protocol: 'ssh',
         host: 'gitlab.com',
         repository: 'gitlab-org/gitlab-ce',
         owner: 'gitlab-org',
@@ -103,7 +163,8 @@ tape('gitlab remotes', function(t) {
     t.end();
 });
 
-tape('local remotes', function(t) {
+tape('ssh and local remotes', function(t) {
+    // local path ending in .git
     t.deepEqual(parseRepo('/full/path/to/repo.git'), {
         remote: '/full/path/to/repo.git',
         protocol: 'file',
@@ -113,6 +174,7 @@ tape('local remotes', function(t) {
         project: 'repo'
     });
 
+    // local path without .git
     t.deepEqual(parseRepo('/full/path/to/repo'), {
         remote: '/full/path/to/repo',
         protocol: 'file',
@@ -120,6 +182,16 @@ tape('local remotes', function(t) {
         repository: 'repo',
         owner: null,
         project: 'repo'
+    });
+
+    // repos/R repository in the user's home directory of another host
+    t.deepEqual(parseRepo('ssh://user@other.host.com/~/repos/R.git'), {
+        remote: 'ssh://user@other.host.com/~/repos/R.git',
+        protocol: 'ssh',
+        host: 'other.host.com',
+        repository: 'R',
+        owner: null,
+        project: 'R'
     });
 
     t.end();
